a501f8215f50615e486121b8ff5557a46ebd13d3
Michael Eder <e1325852@student.tuwien.ac.at>
2017-06-22 21:06:14 +0200

    add sensor java program

---
+++ sensor_smartVienna/src/main/java/Main.java
@@ -0,0 +1,119 @@
+import com.rabbitmq.client.Channel;
+import com.rabbitmq.client.Connection;
+import com.rabbitmq.client.ConnectionFactory;
+import com.rabbitmq.client.QueueingConsumer;
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.io.*;
+import java.net.URL;
+import java.nio.charset.Charset;
+import java.util.HashMap;
+import java.util.Hashtable;
+
+/**
+ * Created by Michael on 02.06.2017.
+ */
+public class Main {
+
+
+    public static void main(String[] args) throws Exception {
+        String uri = System.getenv("CLOUDAMQP_URL");
+        if (uri == null) uri = "amqp://aeuvhavi:n5_ThNGJZ1GZ33gmBJAGglVUoYdvgk0x@orangutan.rmq.cloudamqp.com/aeuvhavi";
+
+        ConnectionFactory factory = new ConnectionFactory();
+        factory.setUri(uri);
+
+        //Recommended settings
+        factory.setRequestedHeartbeat(30);
+        factory.setConnectionTimeout(30000);
+
+        Connection connection = factory.newConnection();
+        boolean durable = false;    //durable - RabbitMQ will never lose the queue if a crash occurs
+        boolean exclusive = false;  //exclusive - if queue only will be used by one connection
+        boolean autoDelete = false; //autodelete - queue is deleted when last consumer unsubscribes
+
+        final Channel consumerQueue = connection.createChannel();
+        final String consumerQueueName = "intervalQueue";
+        consumerQueue.queueDeclare(consumerQueueName, durable, exclusive, autoDelete, null);
+
+        new Thread(new Runnable() {
+            public void run() {
+
+                QueueingConsumer consumer = new QueueingConsumer(consumerQueue);
+
+                try {
+
+                    HashMap<String, Thread> threadHashMap = new HashMap<String, Thread>();
+
+                    while (true) {
+                        System.out.println("start consuming...");
+                        consumerQueue.basicConsume(consumerQueueName, true, consumer);
+
+                        printStatus(threadHashMap);
+
+                        QueueingConsumer.Delivery delivery = consumer.nextDelivery();
+                        String m = new String(delivery.getBody());
+                        System.out.println(m);
+                        JSONObject message = new JSONObject(m);
+                        System.out.println(" [x] Received '" + m.toString() + "'");
+
+                        String rbl = message.get("rbl").toString();
+                        Thread thread = threadHashMap.get(rbl);
+                        if(thread == null && new Integer(message.get("interval").toString()) != 0) {
+                            //Start Request Thread!
+                            RequestThread requestThread = new RequestThread(message.get("rbl").toString(), new Integer(message.get("interval").toString()));
+                            Thread t = new Thread(requestThread);
+
+                            threadHashMap.put(message.get("rbl").toString(), t);
+
+
+                            t.start();
+                        }
+                        else
+                        {
+                            System.out.println("stop thread for rbl " + rbl);
+                            thread.interrupt();
+
+                            threadHashMap.remove(rbl);
+                            String interval = message.get("interval").toString();
+                            int i = Integer.parseInt(interval);
+                            if(i != 0) {
+                                System.out.println("start new thread for rbl " + rbl + " with interval " + message.get("interval").toString());
+                                RequestThread requestThread = new RequestThread(message.get("rbl").toString(), new Integer(message.get("interval").toString()));
+                                Thread t = new Thread(requestThread);
+                                t.setName(message.get("interval").toString());
+                                threadHashMap.put(message.get("rbl").toString(), t);
+                                t.start();
+                            }
+
+                            printStatus(threadHashMap);
+
+                        }
+
+                    }
+                } catch (IOException e) {
+                    e.printStackTrace();
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+            }
+        }).start();
+
+
+
+
+    }
+
+    private static void printStatus(HashMap<String, Thread> threadHashMap)
+    {
+        System.out.println("--------STATUS-----------");
+        for(String rb : threadHashMap.keySet())
+        {
+            Thread thread1  = threadHashMap.get(rb);
+            System.out.println("Running Thread with rbl " + rb + " and interval " + thread1.getName());
+        }
+        System.out.println("-------------------------");
+    }
+}
---
+++ sensor_smartVienna/src/main/java/RequestThread.java
@@ -0,0 +1,125 @@
+import com.rabbitmq.client.Channel;
+import com.rabbitmq.client.Connection;
+import com.rabbitmq.client.ConnectionFactory;
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.io.*;
+import java.net.URISyntaxException;
+import java.net.URL;
+import java.nio.charset.Charset;
+import java.security.KeyManagementException;
+import java.security.NoSuchAlgorithmException;
+
+/**
+ * Created by Michael on 14.06.2017.
+ */
+public class RequestThread implements Runnable {
+
+
+    private String rbl;
+    private int interval;
+
+    public RequestThread(String rbl, int interval)
+    {
+        this.rbl = rbl;
+        this.interval = interval;
+    }
+
+    public void run() {
+
+        try {
+
+            System.out.println("track station with rbl " + rbl + " in interval of " + interval);
+
+            String uri = System.getenv("CLOUDAMQP_URL");
+            if (uri == null) uri = "amqp://aeuvhavi:n5_ThNGJZ1GZ33gmBJAGglVUoYdvgk0x@orangutan.rmq.cloudamqp.com/aeuvhavi";
+
+            ConnectionFactory factory = new ConnectionFactory();
+            factory.setUri(uri);
+
+            //Recommended settings
+            factory.setRequestedHeartbeat(30);
+            factory.setConnectionTimeout(30000);
+
+            Connection connection = factory.newConnection();
+
+            Channel smartVienna = connection.createChannel();
+
+            String queue = "smartViennaQueue";     //queue name
+            boolean durable = false;    //durable - RabbitMQ will never lose the queue if a crash occurs
+            boolean exclusive = false;  //exclusive - if queue only will be used by one connection
+            boolean autoDelete = false; //autodelete - queue is deleted when last consumer unsubscribes
+
+            smartVienna.queueDeclare(queue, durable, exclusive, autoDelete, null);
+
+            //GET info from vienna public transport api
+
+            boolean stop = false;
+            while(!stop) {
+                JSONObject response = readJsonFromUrl("http://www.wienerlinien.at/ogd_realtime/monitor?rbl=" + rbl + "&activateTrafficInfo=stoerungkurz&activateTrafficInfo=stoerunglang&activateTrafficInfo=aufzugsinfo&sender=sXHtcQDfyhDUbYYJ");
+                System.out.println(response.toString());
+                JSONObject data = (JSONObject) response.get("data");
+                JSONArray monitors = (JSONArray) data.get("monitors");
+
+                System.out.println(monitors);
+
+                JSONObject monitorAndInterval = new JSONObject();
+                monitorAndInterval.put("data", monitors);
+                monitorAndInterval.put("interval", interval);
+
+                String exchangeName = "";
+                String routingKey = "smartViennaQueue";
+
+                smartVienna.basicPublish(exchangeName, routingKey, null, monitorAndInterval.toString().getBytes());
+                System.out.println(" [x] Sent details for station '" + rbl + "'");
+
+                try {
+                    Thread.sleep(interval);
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+
+                    //untrack
+                    JSONObject untrack = new JSONObject();
+                    untrack.put("untrack", rbl);
+                    System.out.println(untrack.toString());
+                    smartVienna.basicPublish(exchangeName, routingKey, null, untrack.toString().getBytes());
+
+                    stop = true;
+                }
+            }
+
+        } catch (IOException ioe)
+        {
+            ioe.printStackTrace();
+        } catch (NoSuchAlgorithmException e) {
+            e.printStackTrace();
+        } catch (KeyManagementException e) {
+            e.printStackTrace();
+        } catch (URISyntaxException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public static JSONObject readJsonFromUrl(String url) throws IOException, JSONException {
+        InputStream is = new URL(url).openStream();
+        try {
+            BufferedReader rd = new BufferedReader(new InputStreamReader(is, Charset.forName("UTF-8")));
+            String jsonText = readAll(rd);
+            JSONObject json = new JSONObject(jsonText);
+            return json;
+        } finally {
+            is.close();
+        }
+    }
+
+    private static String readAll(Reader rd) throws IOException {
+        StringBuilder sb = new StringBuilder();
+        int cp;
+        while ((cp = rd.read()) != -1) {
+            sb.append((char) cp);
+        }
+        return sb.toString();
+    }
+}
