109b3453b4e2d465dce2610fe82a220a42fcc12e
Michael Eder <e1325852@student.tuwien.ac.at>
2017-06-22 22:50:17 +0200

    try

--- src/main/webapp/app/vendor.ts
+++ src/main/webapp/app/vendor.ts
@@ -1,6 +1,3 @@
 /* after changing this file run 'yarn run webpack:build:vendor' or 'yarn install' or 'yarn run webpack:build' */
 /* tslint:disable */
 import '../content/css/vendor.css';
-import 'font-awesome/css/font-awesome.min.css';
-import 'primeng/resources/themes/omega/theme.css';
-import 'primeng/resources/primeng.min.css';
